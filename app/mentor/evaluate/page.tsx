0584d14271415ddc1410a86e22cccb3e735e3b3e
tsion <tsionawitua@gmail.com>
2024-03-18 18:41:01 +0300

    pull changes

--- app/mentor/evaluate/page.tsx
+++ app/mentor/evaluate/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const page = () => {
+const questions=[
+  {question:"How was the student's academic and technical background on a scale of 1 to 5", weight:5},
+  {question:"How was the student's academic and technical background on a scale of 1 to 5", weight:15},
+  {question:"How was the student's academic and technical background on a scale of 1 to 5", weight:10},
+  {question:"How was the student's academic and technical background on a scale of 1 to 5", weight:5},
+  {question:"How was the student's academic and technical background on a scale of 1 to 5", weight:25}
+
+];
+
   return (
-    <div>page</div>
+    <div>
+      <div>
+        <p>Evaluation Form</p>
+        <ol>
+          <li>
+            {questions.map((quest)=>(
+              
+            ))}
+          </li>
+        </ol>
+      </div>
+    </div>
   )
 }
 
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -11,7 +11,7 @@ import SidebarLink from "./SidebarLink";
 import { useState } from "react";
 import { getMenu } from "./getmenu";
 
-const role = "collegedean";
+const role = "mentor";
 
 export const menuUtems = getMenu(role);
 const Sidebar = ({ isCollapsed, open, setOpen }: any) => {
