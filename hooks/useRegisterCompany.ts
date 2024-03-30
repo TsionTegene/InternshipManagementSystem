7b1b113fe485ba0094b1200571cd7cbe5d3c659b
Abel <abelzeleke5173@gmail.com>
2024-03-30 10:49:34 +0300

    Update hooks

--- hooks/useRegisterCompany.ts
+++ hooks/useRegisterCompany.ts
@@ -3,5 +3,5 @@ import { RegisterCompany } from "@/queries/company.registration.queries";
 
 export function useRegisterCompany(data: ICompanyRegistrationForm) {
     // To send a data to the react-query mutation, we need to call the mutate function with the data as a parameter
-    const registerCompany = RegisterCompany.m;
+    const
 }
