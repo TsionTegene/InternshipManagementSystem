3772c7fec6690e2592bb2eee18e97552b3d930cd
Abel <86670708+Abel5173@users.noreply.github.com>
2024-04-18 08:12:00 +0000

    login update

--- components/login/login.tsx
+++ components/login/login.tsx
@@ -1,64 +1,50 @@
 "use client";
-import Image from "next/image";
-import bg4 from "@/public/images/bg4.jpg";
-import * as React from "react";
-import "@/app/signup/page";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
+
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"; // this is used to validate the form
+import { useForm } from "react-hook-form"; // this is used to create the form
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+  CardFooter,
+} from "../ui/card";
+import Link from 'next/link';
 import { useAuthenticate } from "@/hooks/useAuthenticate";
-import useSessionStore from "@/stores/sessionStore";
-import { Card, CardHeader, CardContent, FormLabel, FormControl } from "@mui/material";
-import { Form, useForm } from "react-hook-form";
-import { CardTitle, CardDescription } from "../ui/card";
-import { FormField, FormItem, FormMessage } from "../ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
 
 const formSchema = z.object({
-  email: z.string().email({
-    message: "Invalid email address.",
-  }),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
-  }),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
 });
-
 export function Login() {
-  const [formData, setFormData] = React.useState({
-    email: "",
-    password: "",
-  });
-
   const { authenticate, isPending, isError, error } = useAuthenticate();
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Move this to the top to ensure no default form submission
-
-    const getData = new FormData(e.currentTarget);
-    // Extracting data from FormData object
-    const formValues = {
-      email: getData.get("email"),
-      password: getData.get("password"),
-    };
-
-    const formData = new FormData();
-    formData.append("email", formValues.email || "");
-    formData.append("password", formValues.password || "");
-
-    // Call the authenticate function with the validated data
-    const result = authenticate(formData);
-    
-  };
-  const form = useForm({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
   });
 
-const handleOnSubmit = async (data: any) => {
-    console.log(data);
-    const result = await authenticate(data);
+  const onSubmit = async (formValues: z.infer<typeof formSchema>) => {
+    console.log(formValues);
+    const formData = new FormData();
+    formData.append("email", formValues.email);
+    formData.append("password", formValues.password);
+    console.log(formData.get("email"));
+    console.log(formData.get("password"));
+    const result = await authenticate(formData);
     console.log(result);
-  }
-
+  };
   return (
     <>
       <div
@@ -70,68 +56,70 @@ const handleOnSubmit = async (data: any) => {
           zIndex: 0,
         }}
       >
-        <div className="flex flex-col md:flex-row justify-center items-center max-w-7xl mx-auto p-6 bg-white shadow-xl rounded-lg overflow-hidden">
-          <div className="bg-white p-8 rounded-lg shadow-sm max-w-md w-full">
-            <h2 className="text-cyan-700 font-mono text-center mb-2 text-4xl font-extrabold">
-              Welcome Back
-            </h2>
-            <div className="border-b border-cyan-200 pb-4">
-              <h3 className="text-2xl pb-4 font-semibold">Login</h3>
-              <p className="text-sm text-gray-600">
-                Enter your email below to login to your account
-              </p>
-            </div>
-            <form className="space-y-4 mt-4" onSubmit={handleSubmit}>
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700"
-                >
-                  Email
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  name="email"
-                  placeholder="m@example.com"
-                  required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-cyan-500 focus:border-cyan-500"
-                />
-              </div>
-              <div>
-                <div className="flex justify-between items-center">
-                  <label
-                    htmlFor="password"
-                    className="block text-sm font-medium text-gray-700"
-                  >
-                    Password
-                  </label>
-                  <a
-                    href="#"
-                    className="text-sm text-cyan-700 hover:text-cyan-900 underline"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
-                <input
-                  id="password"
-                  type="password"
-                  name="password"
-                  required
-                  className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-cyan-500 focus:border-cyan-500"
-                />
-              </div>
-              <Button
-                type="submit"
-                disabled={isPending}
-                className="w-full bg-cyan-600 text-white text-lg py-2 rounded-md hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
+        <Card>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>
+              Enter your credintials below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
               >
-                {isPending ? "Logging in..." : "Login"}
-              </Button>
-              {isError && error && (
-                <p className="error">Error: {error.message}</p>
-              )}
-            </form>
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="email" {...field} />
+                      </FormControl>
+                      <FormDescription>
+                        Enter your email address
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="password"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormDescription>
+                        Enter your password here
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  disabled={isPending}
+                  className="w-full bg-cyan-600 text-white text-lg py-2 rounded-md hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-opacity-50"
+                >
+                  {isPending ? (<><svg className="animate-spin h-5 w-5 mr-3 ..." viewBox="0 0 24 24"></svg>Logging...</>) : "Login"}
+                </Button>
+                {isError && error && (
+                  <p className="error">Error: {error.message}</p>
+                )}
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter>
             <p className="mt-4 text-center text-sm text-gray-600">
               Don&apos;t have an account?{" "}
               <Link
@@ -141,63 +129,9 @@ const handleOnSubmit = async (data: any) => {
                 Sign up
               </Link>
             </p>
-          </div>
-          <div className="mt-6 md:mt-0 md:ml-6">
-            <Image
-              alt="teaching"
-              className="rounded-lg shadow-xl"
-              src={bg4}
-              width={400}
-              height={300} // Adjusted for better proportion
-            />
-          </div>
-        </div>
+          </CardFooter>
+        </Card>
       </div>
-
-      <Card className="mx-auto max-w-4xl my-10 shadow-2xl rounded-lg overflow-hidden">
-        <CardHeader>
-          <CardTitle className="text-xl">Sign In</CardTitle>
-          <CardDescription>Sign In To Your Account</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleOnSubmit)}
-              className="space-y-8"
-            >
-              <div className="grid gap-5">
-                <div className="grid grid-cols-2 gap-5">
-                  {/* First Name */}
-                  <FormField
-                    control={form.control}
-                    name="firstName"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>
-                          First Name <span className="text-red-700">*</span>
-                        </FormLabel>
-                        <FormControl>
-                          <Input placeholder="First Name" {...field} />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <Button type="submit" className="w-full">
-                  Create an account
-                </Button>
-              </div>
-              <div className="mt-4 text-center text-sm">
-                Already have an account?{" "}
-                <Link href="/login" className="underline">
-                  Sign in
-                </Link>
-              </div>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
     </>
   );
 }
