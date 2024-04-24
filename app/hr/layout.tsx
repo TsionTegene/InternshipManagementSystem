6659fdc11b49e16030c79d873094ca54e9b86270
Abel Zeleke <abelzeleke5173@gmail.com>
2024-04-24 19:16:31 +0000

    trying...

--- app/hr/layout.tsx
+++ app/hr/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import Navbar from "@/components/navbar/navbar";
 import Sidebar from "@/components/sidebar/sidebar";
 import { Button } from "@/components/ui/button";
@@ -9,55 +9,62 @@ import { AlignJustify } from "lucide-react";
 import { IoIosArrowDropleft } from "react-icons/io";
 import { RiMenu4Line } from "react-icons/ri";
 import { useIsLoggedIn } from "@/hooks/useAuthenticate";
+import React, { useLayoutEffect } from "react";
+import { IsAuthenticated } from "@/lib/IsAuthenticated";
+import { useRouter } from "next/navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [open, setOpen] = useState(false);
 
-  // useEffect(() => {
-  //   useIsLoggedIn;
-  // }, [])
+  const router = useRouter();
+  useLayoutEffect(() => {
+    const isAuth = IsAuthenticated("COMPNAY_HR").then((isAuth) => {
+      if (!isAuth) {
+        router.push("/login");
+      }
+    });
+  }, [router]);
 
   return (
-
     // <ProtectedRoute roles={["COMPANY_HR"]}>
-      <div
-        className={`grid  grid-rows-custom  h-screen transition-all duration-300  ${
-          isCollapsed
-            ? "lg:grid-cols-collapsed "
-            : " lg:grid-cols-notCollapsed   "
-        }`}
-      >
-        <header className=" relative  py-3 px-12  rounded-sm ">
-          <div className="flex items-center space-x-5">
-            <div className="lg:hidden py-3">
-              <RiMenu4Line className="size-8" onClick={() => setOpen(!open)} />
-            </div>
-
-            <div className="flex-1">
-              <Navbar />
-            </div>
+    <div
+      className={`grid  grid-rows-custom  h-screen transition-all duration-300  ${
+        isCollapsed
+          ? "lg:grid-cols-collapsed "
+          : " lg:grid-cols-notCollapsed   "
+      }`}
+    >
+      <header className=" relative  py-3 px-12  rounded-sm ">
+        <div className="flex items-center space-x-5">
+          <div className="lg:hidden py-3">
+            <RiMenu4Line className="size-8" onClick={() => setOpen(!open)} />
           </div>
-          <div className="absolute -left-3 top-10  ">
-            <Button
-              onClick={() => setIsCollapsed(!isCollapsed)}
-              className="hidden lg:block rounded-full p-1 z-50 border dark:border-gray-600 border-dashed size-6 text-palesky bg-white dark:bg-background"
-            >
-              <FaAngleRight
-                className={`w-full h-full ${
-                  isCollapsed ? "" : "rotate-180 transition-all duration-600 "
-                }`}
-              />
-            </Button>
+
+          <div className="flex-1">
+            <Navbar />
           </div>
-        </header>
+        </div>
+        <div className="absolute -left-3 top-10  ">
+          <Button
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            className="hidden lg:block rounded-full p-1 z-50 border dark:border-gray-600 border-dashed size-6 text-palesky bg-white dark:bg-background"
+          >
+            <FaAngleRight
+              className={`w-full h-full ${
+                isCollapsed ? "" : "rotate-180 transition-all duration-600 "
+              }`}
+            />
+          </Button>
+        </div>
+      </header>
 
-        <aside className=" hidden lg:block overflow-y-scroll overflow-x-hidden row-custom border-r dark:border-gray-600 border-dashed  ">
-          <Sidebar isCollapsed={isCollapsed} open={open} setOpen={setOpen} />
-        </aside>
+      <aside className=" hidden lg:block overflow-y-scroll overflow-x-hidden row-custom border-r dark:border-gray-600 border-dashed  ">
+        <Sidebar isCollapsed={isCollapsed} open={open} setOpen={setOpen} />
+      </aside>
 
-        <main className="pt-10  px-12 overflow-y-scroll ">{children}</main>
-      </div>
+      <main className="pt-10  px-12 overflow-y-scroll ">{children}</main>
+    </div>
     // </ProtectedRoute>
   );
 };
