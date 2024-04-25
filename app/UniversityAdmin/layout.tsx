f73c79d96628b9a0fb3bc99cb24cf817f4b830e1
Abel <abelzeleke5173@gmail.com>
2024-04-25 16:21:04 +0300

    Conflict Merge.

--- app/UniversityAdmin/layout.tsx
+++ app/UniversityAdmin/layout.tsx
@@ -3,16 +3,25 @@ import Navbar from "@/components/navbar/navbar";
 import Sidebar from "@/components/sidebar/sidebar";
 import { Button } from "@/components/ui/button";
 import { FaAngleRight } from "react-icons/fa";
-import { useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import { AlignJustify } from "lucide-react";
 import { IoIosArrowDropleft } from "react-icons/io";
 import { RiMenu4Line } from "react-icons/ri";
-import ProtectedRoute from "@/lib/ProtectedRoute";
+import { useRouter } from "next/navigation";
+import { IsAuthenticated } from "@/lib/IsAuthenticated";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [open, setOpen] = useState(false);
-
+  const router = useRouter();
+  useLayoutEffect(() => {
+    const isAuth = IsAuthenticated("COMPNAY_HR").then((isAuth) => {
+      if (!isAuth) {
+        console.log("isAuth: ", isAuth);
+        router.push("/login");
+      }
+    });
+  }, []);
   return (
     // <ProtectedRoute roles={["UNIVERSITY_ADMIN"]}>
     <div
@@ -28,31 +37,31 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <RiMenu4Line className="size-8" onClick={() => setOpen(!open)} />
           </div>
 
-            <div className="flex-1">
-              <Navbar />
-            </div>
+          <div className="flex-1">
+            <Navbar />
           </div>
-          <div className="absolute -left-3 top-10  ">
-            <Button
-              onClick={() => setIsCollapsed(!isCollapsed)}
-              className="hidden lg:block rounded-full p-1 z-50 border dark:border-gray-600 border-dashed size-6 text-palesky bg-white dark:bg-background"
-            >
-              <FaAngleRight
-                className={`w-full h-full ${
-                  isCollapsed ? "" : "rotate-180 transition-all duration-600 "
-                }`}
-              />
-            </Button>
-          </div>
-        </header>
+        </div>
+        <div className="absolute -left-3 top-10  ">
+          <Button
+            onClick={() => setIsCollapsed(!isCollapsed)}
+            className="hidden lg:block rounded-full p-1 z-50 border dark:border-gray-600 border-dashed size-6 text-palesky bg-white dark:bg-background"
+          >
+            <FaAngleRight
+              className={`w-full h-full ${
+                isCollapsed ? "" : "rotate-180 transition-all duration-600 "
+              }`}
+            />
+          </Button>
+        </div>
+      </header>
 
-        <aside className=" hidden lg:block overflow-y-scroll overflow-x-hidden row-custom border-r dark:border-gray-600 border-dashed  ">
-          <Sidebar isCollapsed={isCollapsed} open={open} setOpen={setOpen} />
-        </aside>
+      <aside className=" hidden lg:block overflow-y-scroll overflow-x-hidden row-custom border-r dark:border-gray-600 border-dashed  ">
+        <Sidebar isCollapsed={isCollapsed} open={open} setOpen={setOpen} />
+      </aside>
 
       <main className="pt-10  px-12 overflow-y-scroll ">{children}</main>
     </div>
-  // </ProtectedRoute>
+    // </ProtectedRoute>
   );
 };
 
